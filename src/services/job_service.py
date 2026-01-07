50b759447d7b3f9c4ece40757678c5ea5d86d984
Diego Costa <diegohenriquecostasilva01@gmail.com>
2026-01-06 22:15:50 -0300

    Update: function to create CSV files is completed.

--- src/services/job_service.py
+++ src/services/job_service.py
@@ -6,6 +6,12 @@ from sqlalchemy.exc import IntegrityError
 from src.database.connection import SessionLocal
 from src.models.job import Job
 
+import pandas as pd
+import os 
+from datetime import datetime
+
+
+
 def save_job(cleaned_data: dict) -> bool:
     
     session = SessionLocal()
@@ -43,3 +49,55 @@ def show_jobs():
 
     finally:
         session.close()
+
+
+
+def get_jobs_dataframe():
+    session = SessionLocal()
+    try:
+        results = (
+            session.query(
+                Job.id,
+                Job.external_id,
+                Job.title,
+                Job.company,
+                Job.location,
+                Job.salary,
+                Job.created_at,
+                Job.url
+            )
+            .all()
+        ) 
+        # Ajustar isso para retornar no CLI as mensagens
+        if not results:
+            return None
+
+        df = pd.DataFrame(
+            data=results,
+            columns=[
+                "id",
+                "external_id",
+                "title",
+                "company",
+                "location",
+                "salary",
+                "created_at",
+                "url"
+            ]
+        )
+
+        folder_path = os.path.join(os.getcwd(), 'exports/powerbi')
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        
+        date_suffix = datetime.now().strftime("_%d%m%Y_%H%M")
+        original_filename = "jobs.csv"
+        new_filename = original_filename.replace('.csv', f"{date_suffix}.csv")
+
+        destination_path = os.path.join(folder_path, new_filename)
+        df.to_csv(destination_path, index=False)
+
+    finally:
+        session.close()
+        
+    return destination_path
---
+++ teste_loadCSV.py
@@ -0,0 +1,5 @@
+from src.services.job_service import get_jobs_dataframe
+
+teste = get_jobs_dataframe()
+
+print(teste)
